e658b70454298c75a8c4777ab4149894d3f96bce
umamuthu94 <umamuthu1994@gmail.com>
2024-03-12 11:43:54 +0530

    updated pod metrics consumer class

--- src/main/java/com/zaga/kafka/consumer/PodMetricsConsumerService.java
+++ src/main/java/com/zaga/kafka/consumer/PodMetricsConsumerService.java
@@ -18,16 +18,27 @@ public class PodMetricsConsumerService {
     @Inject
     PodMetricDTORepo podMetricDTORepo;
 
-  @Incoming("pod-in")
+  @Incoming("pod-audit-in")
   public void consumePodMetricDetails(OtelPodMetric podMetrics) {
     System.out.println("-------------consumed infra podmetric data----------------"+podMetrics);
     if (podMetrics != null) {
-      podCommandHandler.createPodMetric(podMetrics);
+      // podCommandHandler.createPodMetric(podMetrics);
     podCommandRepo.persist(podMetrics);
     } else {
       System.out.println("Received null message. Check serialization/deserialization.");
     }
   }
+
+    @Incoming("pod-in")
+    public void consumePodDTOMetricDetails(OtelPodMetric podMetrics) {
+      System.out.println("-------------consumed infra podmetricDTO data----------------"+podMetrics);
+      if (podMetrics != null) {
+        podCommandHandler.extractAndMapData(podMetrics);
+      // podCommandRepo.persist(podMetrics);
+      } else {
+        System.out.println("Received null message. Check serialization/deserialization.");
+      }
+    }
 
 
 
