6a54aa5e81daf28067b88726ef277b5fd7e684e1
durga1808 <durgalakshmi3713@gmail.com>
2024-03-20 21:24:26 +0530

    updated on Podmetric persistance

--- src/main/java/com/zaga/controller/PodController.java
+++ src/main/java/com/zaga/controller/PodController.java
@@ -2,8 +2,17 @@ package com.zaga.controller;
 
 
 
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import com.zaga.entity.pod.OtelPodMetric;
 import com.zaga.handler.PodCommandHandler;
+import com.zaga.kafka.consumer.PodMetricsConsumerService;
 import com.zaga.repo.PodCommandRepo;
 import com.zaga.repo.PodMetricDTORepo;
 
@@ -29,6 +38,14 @@ PodCommandHandler podCommandHandler;
 @Inject
 PodMetricDTORepo podMetricDTORepo;
 
+@Inject
+PodMetricsConsumerService podMetricsConsumerService;
+
+@Inject
+@ConfigProperty(name ="cluster.url")
+String clusterurl;
+
+
 @POST
 @Path("/create")
 public Response createPodMetric(OtelPodMetric podMetric) {
@@ -36,4 +53,53 @@ public Response createPodMetric(OtelPodMetric podMetric) {
     podCommandHandler.extractAndMapData(podMetric);
     return Response.status(Response.Status.CREATED).entity(podMetric).build();
 }
+
+
+
+// @POST
+// @Path("/create-pod")
+// public Response createPodMetrics(OtelPodMetric podMetric) {
+//     String Url= "https://api.zagaobservability.zagaopensource.com:6443/podMetrics/create-pod";
+//     replicateData(podMetric, Url);
+
+
+// }
+
+//     private void replicateData(OtelPodMetric podData, String destinationUrl) {
+//         try {
+//             HttpClient client = HttpClient.newHttpClient();
+//             HttpRequest request = HttpRequest.newBuilder()
+//                     .uri(new URI(destinationUrl))
+//                     .POST(HttpRequest.BodyPublishers.ofString(podData.toString()))
+//                     .build();
+//             client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+//             System.out.println("##########################-------"+podData);
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
+
+
+    @POST
+    @Path("/create_pod")
+    public void createEvents (OtelPodMetric metric){
+      String Url = "https://api.zagaobservability.zagaopensource.com:6443/podMetrics/create_pod";
+      replicateData(metric,Url);
+         podMetricsConsumerService.consumePodMetricDetails(metric);
+    }
+
+    private void replicateData(OtelPodMetric metric, String destinationUrl) {
+              try {
+                  HttpClient client = HttpClient.newHttpClient();
+                  HttpRequest request = HttpRequest.newBuilder()
+                          .uri(new URI(destinationUrl))
+                          .POST(HttpRequest.BodyPublishers.ofString(metric.toString()))
+                          .build();
+                  client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+                     System.out.println("**********start"+metric);   
+              } catch (Exception e) {
+                  e.printStackTrace();
+              }
+          }
+
 }
